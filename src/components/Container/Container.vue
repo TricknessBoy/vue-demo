1ea5d04a164bbcc5f8e2ee9a37a0c55b7be0ed36
dongmingman <dongmingman@bcpeople.com>
2018-07-19 14:07:14 +0800

    modify some text
    Signed-off-by: dongmingman <dongmingman@bcpeople.com>

--- src/components/Container/Container.vue
+++ src/components/Container/Container.vue
@@ -1,21 +1,29 @@
 <template>
-  <div class="container">
-    <div class="container-warpper">
-      <ul class="menu-list">
-        <li v-for="item in items" :key="item.id">
-          <!--<router-link :to="'/page/'+item.id" :params="item">标题：{{item.title}}</router-link>-->
-          <router-link :to="{name:'list',params:item}">标题：{{item.title}}</router-link>
-        </li>
-      </ul>
-      <div class="list"><router-link :to="{path:'/demo/20180609'}">2018年06月09日-简单开关</router-link></div>
-      <div class="list"><router-link :to="{path:'/demo/20180610'}">2018年06月10日-简单Tab切换</router-link></div>
-      <div class="list"><router-link :to="{path:'/demo/20180612'}">2018年06月12日-简单轮播图</router-link></div>
-      <div class="list"><router-link :to="{path:'/demo/20180718'}">2018年07月18日-MintUI之Toast组件</router-link></div>
-        <div class="list">
-            <router-link :to="{path:'/demo/20180718/scss'}">2018年07月18日-Scss之Toast组件</router-link>
+    <div class="container">
+        <div class="container-warpper">
+            <ul class="menu-list">
+                <li v-for="item in items" :key="item.id">
+                    <!--<router-link :to="'/page/'+item.id" :params="item">标题：{{item.title}}</router-link>-->
+                    <router-link :to="{name:'list',params:item}">标题：{{item.title}}</router-link>
+                </li>
+            </ul>
+            <div class="list">
+                <router-link :to="{path:'/demo/20180609'}">2018年06月09日-简单开关</router-link>
+            </div>
+            <div class="list">
+                <router-link :to="{path:'/demo/20180610'}">2018年06月10日-简单Tab切换</router-link>
+            </div>
+            <div class="list">
+                <router-link :to="{path:'/demo/20180612'}">2018年06月12日-简单轮播图</router-link>
+            </div>
+            <div class="list">
+                <router-link :to="{path:'/demo/20180718'}">2018年07月18日-MintUI之Toast组件</router-link>
+            </div>
+            <div class="list">Ω
+                <router-link :to="{path:'/demo/20180718/scss'}">2018年07月18日-Scss之Toast组件</router-link>
+            </div>
         </div>
     </div>
-  </div>
 </template>
 
 <script>
--- src/components/SimpleSwitch/SimpleSwitch.vue
+++ src/components/SimpleSwitch/SimpleSwitch.vue
@@ -1,88 +1,93 @@
 <template>
     <span
-      class="ui-switch"
-      :class="{'ui-switch-on' : slefChecked}"
-      :value="value"
-      @click="toggle"
+            class="ui-switch"
+            :class="{'ui-switch-on' : slefChecked}"
+            :value="value"
+            @click="toggle"
     >
     </span>
 </template>
 
 <script>
-export default {
-  name: 'SimpleSwitch',
-  props: {
-    value: {
-      type: Boolean,
-      default: true
-    }
-  },
-  data () {
-    return {
-      slefChecked: this.value
-    }
-  },
-  watch: {
-    slefChecked (val) {
-      this.$emit('input', val)
-    }
-  },
-  methods: {
-    toggle () {
-      this.slefChecked = !this.slefChecked
-    }
-  }
-}
+	export default {
+		name: 'SimpleSwitch',
+		props: {
+			value: {
+				type: Boolean,
+				default: true
+			}
+		},
+		data() {
+			return {
+				slefChecked: this.value
+			}
+		},
+		watch: {
+			slefChecked(val) {
+				this.$emit('input', val)
+			}
+		},
+		methods: {
+			toggle() {
+				this.slefChecked = !this.slefChecked
+			}
+		}
+	}
 </script>
 
 <style scoped>
-  .ui-switch{
-    display: block;
-    position: relative;
-    width: 62px;
-    height: 32px;
-    margin: 10px auto;
-    border: 1px solid #DFDFDF;
-    outline: 0;
-    border-radius: 16px;
-    box-sizing: border-box;
-    background-color: #DFDFDF;
-    transition: background-color 0.1s, border 0.1s;
-    cursor: pointer;
-  }
-  .ui-switch:before {
-    content: " ";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 60px;
-    height: 30px;
-    border-radius: 15px;
-    background-color: #FDFDFD;
-    transition: transform 0.35s cubic-bezier(0.45, 1, 0.4, 1);
-  }
-  .ui-switch:after {
-    content: " ";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    background-color: #FFFFFF;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.4);
-    transition: transform 0.35s cubic-bezier(0.4, 0.4, 0.25, 1.35);
-  }
-  .ui-switch-on {
-    border-color: #1AAD19;
-    background-color: #1AAD19;
-  }
-  .ui-switch-on:before {
-    border-color: #1AAD19;
-    background-color: #1AAD19;
-  }
-  .ui-switch-on:after {
-    -webkit-transform: translateX(30px);
-    transform: translateX(30px);
-  }
+    .ui-switch {
+        display: block;
+        position: relative;
+        width: 62px;
+        height: 32px;
+        margin: 10px auto;
+        border: 1px solid #DFDFDF;
+        outline: 0;
+        border-radius: 16px;
+        box-sizing: border-box;
+        background-color: #DFDFDF;
+        transition: background-color 0.1s, border 0.1s;
+        cursor: pointer;
+    }
+
+    .ui-switch:before {
+        content: " ";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 60px;
+        height: 30px;
+        border-radius: 15px;
+        background-color: #FDFDFD;
+        transition: transform 0.35s cubic-bezier(0.45, 1, 0.4, 1);
+    }
+
+    .ui-switch:after {
+        content: " ";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        background-color: #FFFFFF;
+        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.4);
+        transition: transform 0.35s cubic-bezier(0.4, 0.4, 0.25, 1.35);
+    }
+
+    .ui-switch-on {
+        border-color: #1AAD19;
+        background-color: #1AAD19;
+    }
+
+    .ui-switch-on:before {
+        border-color: #1AAD19;
+        background-color: #1AAD19;
+    }
+
+    .ui-switch-on:after {
+        -webkit-transform: translateX(30px);
+        transform: translateX(30px);
+    }
 </style>
